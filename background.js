8a6372c66eb7aafa85ab40a2f16288c9e0e50c93
Fontaine Brahima <fontainebrahima@gmail.com>
2025-12-07 23:19:03 +0000

    modification de l'arriere et ajout de sphere

--- background.js
+++ background.js
@@ -6,9 +6,12 @@ let spheres = [];
 
 // Colors from CSS variables (hardcoded here for JS access)
 const colors = [
-    'rgba(149, 255, 100, 0.6)', // Red-ish
-    'rgba(149, 255, 100, 0.6)', // Blue-ish
-    'rgba(100, 255, 100, 0.6)'  // Green-ish
+    'rgba(255, 103, 100, 0.6)', // Red-ish
+    'rgba(100, 110, 255, 0.6)', // Blue-ish
+    'rgba(100, 255, 100, 0.6)',  // Green-ish
+    'rgba(117, 39, 153, 0.6)', // Red-ish
+    'rgba(243, 43, 193, 0.6)', // Blue-ish
+    'rgba(232, 191, 43, 0.6)'  // Green-ish
 ];
 
 class Sphere {
@@ -57,6 +60,9 @@ function init() {
     spheres.push(new Sphere(width * 0.2, height * 0.2, 200, colors[0]));
     spheres.push(new Sphere(width * 0.8, height * 0.8, 150, colors[1]));
     spheres.push(new Sphere(width * 0.5, height * 0.5, 100, colors[2]));
+    spheres.push(new Sphere(width * 0.2, height * 0.2, 200, colors[3]));
+    spheres.push(new Sphere(width * 0.8, height * 0.8, 150, colors[4]));
+    spheres.push(new Sphere(width * 0.5, height * 0.5, 100, colors[5]));
 }
 
 function resize() {
